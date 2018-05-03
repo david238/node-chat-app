ab10e423f1bdf5e8aacce007326dc5b896a06f9e
davidtin <davidtin@opdaconsulting.com>
2018-05-03 12:35:22 -0400

    Add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,18 +1,29 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
-
-console.log(__dirname + '../public');
-console.log(publicPath);
-
-var app = express();
 const port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+//popular event: connection
+io.on('connection', (socket) => {
+    console.log('New User connected');
+    socket.on('disconnect', ()=> {
+      console.log('Client has disconnected');
+    });
+
+});
+
+server.listen(port, () => {
   console.log(`Started on Port ${port}`);
 });
 
-//setup github
+//create a herolu application and deploy to it.
+//visit heroku app on browser.
